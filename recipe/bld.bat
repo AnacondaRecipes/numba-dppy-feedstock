9f7dcf2215fff1651fe787e9c2d5b7192ffe6def
Anthony DiPietro <adipietro@anaconda.com>
2022-02-23 11:56:18 -0500

    Initial update to v0.17.4

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,7 @@
-echo "Activating oneAPI compiler environment..."
-call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
-if errorlevel 1 exit 1
-REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted.
-set ERRORLEVEL=
-
-echo on
+@REM new llvm-spirv location
+@REM starting from dpcpp_impl_win-64=2022.0.0=intel_3638 location is env\Library\bin-llvm
+@REM used BUILD_PREFIX as compiler installed in build section of meta.yml
+set "PATH=%BUILD_PREFIX%\Library\bin-llvm;%PATH%"
 
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 
