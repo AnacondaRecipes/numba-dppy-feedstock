3341a39479bfd403c4d30b71f45e46b546f21749
Anthony DiPietro <51377278+adipietro18@users.noreply.github.com>
2022-03-08 13:38:18 -0500

    Release v0.17.4 (#1)
    * Initial update to v0.17.4
    
    * Recipe updates and clean-ups.
    
    * Add patch for level zero GPU test skip.
    
    * Add Windows `--error-over*` clean-ups..

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-set -e
-
-# For activating OpenCL CPU
-source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
-source ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
-
-set -x
+set -euxo pipefail
 
 pytest -q -ra --disable-warnings --pyargs numba_dppy -vv
 
-NUMBA_DPPY_LLVM_SPIRV_ROOT=${ONEAPI_ROOT}/compiler/latest/linux/bin/ pytest -q -ra --disable-warnings --pyargs numba_dppy.tests.kernel_tests.test_atomic_op::test_atomic_fp_native -vv
+if [[ -v ONEAPI_ROOT ]]; then
+    set +u
+    # shellcheck disable=SC1091
+    source "${ONEAPI_ROOT}/compiler/latest/env/vars.sh"
+    set -u
+
+    export NUMBA_DPPY_LLVM_SPIRV_ROOT="${ONEAPI_ROOT}/compiler/latest/linux/bin"
+    echo "Using llvm-spirv from oneAPI"
+else
+    echo "Using llvm-spirv from dpcpp package in conda testing environment"
+fi
+
+export NUMBA_DPPY_ACTIVATE_ATOMICS_FP_NATIVE=1
+
+pytest -q -ra --disable-warnings -vv \
+    --pyargs numba_dppy.tests.kernel_tests.test_atomic_op::test_atomic_fp_native
 
 exit 0
